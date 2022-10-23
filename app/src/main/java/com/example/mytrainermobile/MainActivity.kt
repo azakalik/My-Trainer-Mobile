1d2af77c29c0a71ece7e2c75a8e0a1b3eb2dab08
sranucci <sranucci@itba.edu.ar>
2022-10-22 23:32:15 -0300

    created intro screen prototype
    created intro screen prototype, it uses values from XML for easier future switching from light theme to dark, and from spanish to english

--- app/src/main/java/com/example/mytrainermobile/MainActivity.kt
+++ app/src/main/java/com/example/mytrainermobile/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.mytrainermobile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mytrainermobile.ui.theme.MyTrainerMobileTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,12 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyTrainerMobileTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                DefaultPreview()
             }
         }
     }
@@ -37,7 +38,18 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MyTrainerMobileTheme {
-        Greeting("Android")
-    }
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.blackMainTheme)), verticalArrangement = Arrangement.SpaceEvenly, horizontalAlignment = Alignment.CenterHorizontally ) {
+            Text(text = "My Trainer",color = Color.Magenta, fontSize = 60.sp)
+            Text(text = stringResource(id = R.string.introScreenMessage), color = Color.White, fontSize = 30.sp)
+            Button(onClick = { /*TODO enviar a pagina correspondiente*/ }, shape = RoundedCornerShape(10.dp), colors = ButtonDefaults.buttonColors(backgroundColor = Color.Magenta)
+            , modifier = Modifier.fillMaxWidth(0.5F)) {
+                Text(text = stringResource(id = R.string.singInButton), fontSize = 30.sp)
+            }
+            Button(onClick = { /*TODO enviar a pagina correspondiente*/ }, shape = RoundedCornerShape(10.dp), colors = ButtonDefaults.buttonColors(backgroundColor = Color.Magenta)
+                , modifier = Modifier.fillMaxWidth(0.5F)) {
+                Text(text = stringResource(id = R.string.signUpButton),color = Color.White, fontSize = 30.sp)
+            }
+        }
 }
