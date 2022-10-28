ad64cb805256084c8a363462d53deda99d739de0
Juan Ignacio Causse <jcausse@itba.edu.ar>
2022-10-28 10:00:12 -0300

    Added INTERNET permission, new dependency and updated ExerciseView

--- app/src/main/java/com/example/mytrainermobile/ExerciseViewActivity.kt
+++ app/src/main/java/com/example/mytrainermobile/ExerciseViewActivity.kt
@@ -13,6 +13,12 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.outlined.PlayArrow
+import androidx.compose.material.icons.outlined.Share
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -45,11 +51,87 @@ class ExerciseViewActivity : ComponentActivity() {
 @Composable
 @Preview(showBackground = true)
 fun ExerciseView() {
-    Column(modifier = Modifier
-    .fillMaxSize()
-    .background(color = colorResource(id = R.color.blackMainTheme)),
-    verticalArrangement = Arrangement.SpaceEvenly,
-    horizontalAlignment = Alignment.CenterHorizontally ) {
-        Text(text = "Current Exercise", color = Color.Magenta, fontSize = 60.sp)
+    Column(
+    modifier = Modifier
+        .fillMaxSize()
+        .background(color = colorResource(id = R.color.blackMainTheme)),
+    verticalArrangement = Arrangement.SpaceBetween,
+    horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .weight(1F)
+            .background(color = Color.Black)
+        ) {
+            Text(text = "Current Exercise", color = Color.Magenta, fontSize = 24.sp)
+        }
+
+        Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .weight(10F)
+            .background(color = Color.White)
+        ) {
+            Text(text = "Picture", color = Color.Black, fontSize = 60.sp)
+        }
+
+        Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .weight(1F)
+            .background(color = Color.Black)
+        ) {
+
+            Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.Transparent),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Button(
+                onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Magenta)
+                ) {
+                    Icon(
+                        Icons.Filled.ArrowBack,
+                        contentDescription = "Previous Exercise",
+                        modifier = Modifier.size(33.dp)
+                    )
+                }
+
+                Button(
+                onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Magenta)
+                ) {
+                    Icon(
+                        Icons.Outlined.PlayArrow,
+                        contentDescription = "Previous Exercise",
+                        modifier = Modifier.size(33.dp)
+                    )
+                }
+
+                Button(
+                onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Magenta)
+                ) {
+                    Icon(
+                        Icons.Filled.ArrowForward,
+                        contentDescription = "Previous Exercise",
+                        modifier = Modifier.size(33.dp)
+                    )
+                }
+
+            }
+
+        }
+
     }
 }
+
