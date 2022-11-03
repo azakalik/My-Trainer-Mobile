c72c16541954702e33e5ae43e272798e6af6884d
mrojaspelliccia <mrojaspelliccia@itba.edu.ar>
2022-11-03 19:46:45 -0300

    preparing for navigation

--- app/src/main/java/com/example/mytrainermobile/MainActivity.kt
+++ app/src/main/java/com/example/mytrainermobile/MainActivity.kt
@@ -1,36 +1,11 @@
 package com.example.mytrainermobile
 
-import android.annotation.SuppressLint
+
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.activity.ComponentActivity
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.mytrainermobile.classes.Routine
 import com.example.mytrainermobile.screens.ArriveScreen
-import com.example.mytrainermobile.screens.WorkoutView
 import com.example.mytrainermobile.ui.theme.MyTrainerMobileTheme
-import kotlin.math.round
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
