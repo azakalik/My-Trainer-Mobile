1971a80a355553458d6c87d4a33d53d52594c193
sranucci <sranucci@itba.edu.ar>
2022-11-14 19:21:09 -0300

    advanced with interface

---
+++ app/src/main/java/com/example/mytrainermobile/screenStates/RoutinesUIState.kt
@@ -0,0 +1,7 @@
+package com.example.mytrainermobile.screenStates
+
+import com.example.mytrainermobile.data.model.Routine
+
+data class RoutinesUIState (
+    val routineList: List<Routine> = listOf()
+)
--- app/src/main/java/com/example/mytrainermobile/viewModels/MyRoutinesViewModel.kt
+++ app/src/main/java/com/example/mytrainermobile/viewModels/MyRoutinesViewModel.kt
@@ -1,14 +1,42 @@
 package com.example.mytrainermobile.viewModels
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.mytrainermobile.data.model.Routine
 import com.example.mytrainermobile.data.network.repository.RoutineRepository
+import com.example.mytrainermobile.screenStates.RoutinesUIState
 import com.example.mytrainermobile.screens.DefaultViewModelInterface
+import com.example.mytrainermobile.util.SessionManager
 
-class MyRoutinesViewModel() : ViewModel(), DefaultViewModelInterface {
-    private var routineList = null
+class MyRoutinesViewModel(val routineRepository: RoutineRepository) : ViewModel(), DefaultViewModelInterface {
+
+    var uiState by mutableStateOf( RoutinesUIState() )
+        private set
+
 
     override fun getRoutines() : List<Routine>{
-        return listOf()
+        return uiState.routineList
     }
+
+
+    override suspend fun loadRoutines() {
+        //por ahroa busca a la API solo una vez, estamos sin refresh
+        uiState = uiState.copy(routineList = routineRepository.getRoutines() )
+    }
+//
+//    override suspend fun makeFavourite(idx:Int) {
+//        TODO("LLamar a favourite repository")
+//    }
+//
+//    override suspend fun removeFavourite(idx:Int) {
+//        TODO("llamar a favourite repository")
+//    }
+//
+//    override suspend fun accessRoutine(idx:Int): Int {
+//        return uiState[idx].id
+//    }
+
+
 }
