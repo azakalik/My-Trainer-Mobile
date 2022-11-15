3de6958f2d05f6f16e211d64b25bfe5c2a25b943
sranucci <sranucci@itba.edu.ar>
2022-11-15 08:02:44 -0300

    Revert "advanced with interface"
    This reverts commit 1971a80a355553458d6c87d4a33d53d52594c193.

--- app/src/main/java/com/example/mytrainermobile/screenStates/RoutinesUIState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mytrainermobile.screenStates
-
-import com.example.mytrainermobile.data.model.Routine
-
-data class RoutinesUIState (
-    val routineList: List<Routine> = listOf()
-)
--- app/src/main/java/com/example/mytrainermobile/screens/DefaultViewModelInterface.kt
+++ app/src/main/java/com/example/mytrainermobile/screens/DefaultViewModelInterface.kt
@@ -4,12 +4,4 @@ import com.example.mytrainermobile.data.model.Routine
 
 interface DefaultViewModelInterface {
     fun getRoutines() : List<Routine>
-
-    suspend fun loadRoutines()
-//
-//    suspend fun makeFavourite(idx:Int)
-//
-//    suspend fun removeFavourite(idx:Int)
-//
-//    suspend fun accessRoutine(idx:Int) : Int
 }
