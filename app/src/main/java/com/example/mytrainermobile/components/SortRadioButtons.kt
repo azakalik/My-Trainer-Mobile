c4d61a34d89fe8bca3013da13494ce7dd4dfbc5a
mrojaspelliccia <mrojaspelliccia@itba.edu.ar>
2022-11-17 14:37:36 -0300

    Refactored popup to open eon screen's centre

---
+++ app/src/main/java/com/example/mytrainermobile/components/SortRadioButtons.kt
@@ -0,0 +1,82 @@
+package com.example.mytrainermobile.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.dp
+import com.example.mytrainermobile.aux_functions.getSortOptionsList
+import com.example.mytrainermobile.ui.theme.DefaultColor
+import com.example.mytrainermobile.viewModels.DefaultViewModelInterface
+
+
+@Composable
+fun ShowRadioButtons(viewModel: DefaultViewModelInterface){
+    val state = viewModel.getState()
+    val radioOptions = state.sortOptions
+    val stringNames = getSortOptionsList()
+    var iterator = 0
+
+    Row() {
+        Column(Modifier.selectableGroup()) {
+            radioOptions.forEach { text ->
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(56.dp)
+                        .selectable(
+                            selected = (text == state.auxSortingBy),
+                            onClick = { viewModel.toggleAuxSortingBy(text) },
+                            role = Role.RadioButton
+                        )
+                        .padding(horizontal = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RadioButton(
+                        selected = (text == state.auxSortingBy),
+                        onClick = null, // null recommended for accessibility with screenreaders
+                        colors = RadioButtonDefaults.colors(selectedColor = DefaultColor)
+                    )
+                    Text(
+                        text = stringNames[iterator++],
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.padding(start = 16.dp),
+                        color = Color.White
+                    )
+                }
+            }
+        }
+    }
+
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .height(56.dp)
+            .padding(horizontal = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Switch(
+            modifier = Modifier
+                .semantics { contentDescription = "Toggle sort descendingly" },
+            checked = state.auxSortDescending,
+            onCheckedChange = { viewModel.toggleAuxSortDescending() },
+            colors = SwitchDefaults.colors(
+                checkedTrackColor = DefaultColor,
+                uncheckedTrackColor = Color.Transparent
+            )
+        )
+        Text(
+            text = "Descending",
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(start = 16.dp),
+            color = Color.White
+        )
+    }
+}
--- app/src/main/java/com/example/mytrainermobile/screens/DefaultShowRoutinesScreen.kt
+++ app/src/main/java/com/example/mytrainermobile/screens/DefaultShowRoutinesScreen.kt
@@ -2,25 +2,31 @@ package com.example.mytrainermobile.screens
 
 import android.annotation.SuppressLint
 import android.content.res.Configuration
-import android.util.Log
-import android.view.Surface
+import androidx.compose.material.IconButton
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material.Scaffold
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Popup
+import com.example.mytrainermobile.R
 import com.example.mytrainermobile.components.*
-import com.example.mytrainermobile.data.model.Routine
+import com.example.mytrainermobile.ui.theme.DefaultBackground
+import com.example.mytrainermobile.ui.theme.DefaultColor
 import com.example.mytrainermobile.ui.theme.MyTrainerMobileTheme
 import com.example.mytrainermobile.viewModels.DefaultViewModelInterface
-import com.example.mytrainermobile.viewModels.ToggleFavouriteViewModelnterface
 
 //This class is only meant to be used by explore, favourites and my routines
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -53,7 +59,16 @@ fun DefaultShowRoutinesScreen(
                             TitleBox(
                                 title = title
                             )
-                            SortButton(viewModel)
+                            IconButton(onClick = {
+                                viewModel.toggleShowSortFAB()
+                            }) {
+                                androidx.compose.material.Icon(
+                                    modifier = Modifier.size(35.dp),
+                                    imageVector = Icons.Filled.Menu,
+                                    contentDescription = "Menu",
+                                    tint = DefaultColor,
+                                )
+                            }
                         }
                     }
                 }
@@ -69,7 +84,16 @@ fun DefaultShowRoutinesScreen(
                             title = title
                         )
                         TitleForSection()
-                        SortButton(viewModel)
+                        IconButton(onClick = {
+                            viewModel.toggleShowSortFAB()
+                        }) {
+                            androidx.compose.material.Icon(
+                                modifier = Modifier.size(35.dp),
+                                imageVector = Icons.Filled.Menu,
+                                contentDescription = "Menu",
+                                tint = DefaultColor,
+                            )
+                        }
                     }
                 }
             }
@@ -92,6 +116,55 @@ fun DefaultShowRoutinesScreen(
                     }
                 }
             )
+            if (viewModel.getState().showSortFAB) {
+                Popup(
+                    alignment = Alignment.Center,
+                    onDismissRequest = { viewModel.toggleShowSortFAB() },
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth(0.6f)
+                            .border(
+                                width = 2.dp,
+                                color = DefaultColor,
+                                shape = RoundedCornerShape(15.dp)
+                            )
+                            .background(color = DefaultBackground, shape = RoundedCornerShape(15.dp))
+                    ) {
+                        Column(
+                            Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            // --------- POPUP TITLE --------------------------------
+                            Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                                Text(
+                                    text = stringResource(id = R.string.fab_name),
+                                    color = Color.White,
+                                    modifier = Modifier.padding(10.dp),
+                                    fontSize = 20.sp
+                                )
+                            }
+                            ShowRadioButtons(viewModel)
+                            // ---------- POPUP BUTTONS ----------------------------
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceEvenly
+                            ) {
+                                DefaultButton(
+                                    onClick = { viewModel.toggleShowSortFAB(); viewModel.unSaveChanges() },
+                                    text = stringResource(id = R.string.cancel)
+                                )
+                                DefaultButton(
+                                    onClick = {
+                                        viewModel.toggleShowSortFAB(); viewModel.saveChanges()
+                                    },
+                                    text = stringResource(id = R.string.accept)
+                                )
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
